9bbc7a6f78ca58f774f4a63a11cdbd4714d4c9f1
Dylan-56 <127716609+Dylan-56@users.noreply.github.com>
2024-07-02 15:06:07 +0200

    close #3 taille fenetre

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import {HELLO_WORLD_TEXT, HELLO_WORLD_TITLE} from "./constant.js";
 
 document.querySelector('#app').innerHTML = `
 //#1 :  La couleur de fond doit etre cyan #1 
-  <div style="background-color: cyan;">
+  <div style="background-color: cyan;
+  weight:150px;
+  height:150px;">
         <p>hello</p>
         <p style="color: blue;">${HELLO_WORLD_TEXT}</p>
         <p>Oh no an other modification</p>

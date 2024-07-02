9ab1ebf845df6ea200333b321ddf0976d6501002
Dylano2 <54356269+Dylano2@users.noreply.github.com>
2024-07-02 14:36:57 +0200

    C20 - commit on github

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ document.querySelector('#app').innerHTML = `
   <div style="background-color: green;">
         <p>hello</p>
         <p style="color: blue;">${HELLO_WORLD_TEXT}</p>
+        <p>Oh no an other modification</p>
   </div>
 `
 

47a27a2337f2b4cba57746024cd502bf6985d8a5
dylan Gonzalez <dylan@tkorp.com>
2024-07-02 09:42:32 +0200

    C6 : couleur de fond en vert (fix_XXX

--- main.js
+++ main.js
@@ -2,13 +2,10 @@ import './style.css'
 import {HELLO_WORLD_TEXT, HELLO_WORLD_TITLE} from "./constant.js";
 
 document.querySelector('#app').innerHTML = `
-  <div style="border: solid red 1px;
-  width: 200px;
-  height: 200px;
-  background-color: yellow;
-  color: black">
+
+  <div style="background-color: green;">
         <p>hello</p>
-        <p>${HELLO_WORLD_TEXT}</p>
+        <p style="color: blue;">${HELLO_WORLD_TEXT}</p>
   </div>
 `
 

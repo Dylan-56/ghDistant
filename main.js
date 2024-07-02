9168c6c58801fbaab6f6077021955ea2f75a86ef
dylan Gonzalez <dylan@tkorp.com>
2024-07-02 09:28:41 +0200

    : mise à jour taille fenetre et ajustement du texte des constantes

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import './style.css'
 import {HELLO_WORLD_TEXT, HELLO_WORLD_TITLE} from "./constant.js";
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div style="border: solid red 1px;
+  width: 200px;
+  height: 200px">
         <p>hello</p>
         <p>${HELLO_WORLD_TEXT}</p>
   </div>
